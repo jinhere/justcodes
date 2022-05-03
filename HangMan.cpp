bd8e5ce29456f980760a3d321010d2abd6c7071c
jinhere <74696590+jinhere@users.noreply.github.com>
2022-05-04 00:19:02 +0900

    Update HangMan.cpp

--- HangMan.cpp
+++ HangMan.cpp
@@ -1,24 +1,30 @@
 // 현재는 단어를 정해줘야 함. 단어 리스트(txt) 파일에서 단어를 받아올 수 없음
 
-cout << "Hang-man";
-cout << "1) play 2) quit\n";
-int game;cin >> game;
-    if (game == 1) {
-        cout << "playing game..\n";
-        string word = "bronze";
-        string uword=word;
-        for (int i = 0; i < word.size(); i++)
-            uword[i] = '-';
-        do{
-            char guess;
-            cout << "TODAY'S WORD : " << uword << endl;
-            cout << "Your guess: "; cin >> guess;
-            for (int i = 0; i < word.size(); i++) {
-                if (word[i] == guess)
-                    uword[i] = guess;
-            }
-        } while (uword != word);
-        cout << uword;
-    }
-    cout <<endl<<"end of the game.";
-return 0;
+#include <iostream>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	string wordlist[] = { "c++","python","javascript" };
+	string answer; char w;
+	srand(time(NULL));
+	int n = rand() % 3;
+	answer = wordlist[n];
+	string guess(answer.length(), '_');
+	while (true) {
+		cout << guess << endl;
+		cout << "enter a letter: "; cin >> w;
+		for (int i = 0; i < answer.length(); i++) {
+			if (w == answer[i])
+				guess[i] = w;
+		}
+		if (guess == answer) {
+			cout << answer << endl << "성공!!";
+			break;
+		}
+			
+	}
+	return 0;
+}
